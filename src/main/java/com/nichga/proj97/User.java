d8a82216ae69074385edcf417aa50b8495df37b7
Extaseq <daisuki.inc.cpn@gmail.com>
2024-10-14 17:52:39 +0700

    Update CMD

--- src/main/java/com/nichga/proj97/User.java
+++ src/main/java/com/nichga/proj97/User.java
@@ -2,7 +2,7 @@ package com.nichga.proj97;
 
 import java.util.*;
 
-public class Users {
+public class User {
 
     private String name;
 
@@ -34,20 +34,26 @@ public class Users {
     /**
      * Default Constructor
      */
-    Users() {
+    User() {
         name = "Unknown";
     }
 
-    Users(String name, List<Documents> doc) {
+    User(int id, String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    User( String name, List<Documents> doc) {
+        this.id = id;
         this.name = name;
         userDocuments = doc;
     }
 
-    Users(String name) {
+    User(String name) {
         this.name = name;
     }
 
-    Users(List<Documents> doc) {
+    User(List<Documents> doc) {
         name = "Unknown";
         userDocuments = doc;
     }
@@ -67,6 +73,10 @@ public class Users {
     void setUserDocuments(List<Documents> doc) {
         userDocuments.clear();
         userDocuments.addAll(doc);
+    }
+
+    List<Documents> getDocumentList() {
+        return userDocuments;
     }
 
     void setRecommendDoc(List<Documents> doc) {
