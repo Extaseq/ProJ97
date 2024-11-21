1539f306982668c94191cca49597454d6bc0dd19
ledat5934 <ledat5934@gmail.com>
2024-11-21 17:47:42 +0700

    Create BookRepository.java

---
+++ src/main/java/com/nichga/proj97/Database/BookRepository.java
@@ -0,0 +1,34 @@
+package com.nichga.proj97.Database;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookRepository extends GenericRepository {
+    public BookRepository() {
+        super("books");
+    }
+    public static class Column extends GenericColumn {
+        public static final List<Column> columns = new ArrayList<>();
+        public static final Column AUTHOR_ID = new Column(1,  "author_id");
+        public static final Column BOOK_ID  = new Column(2,  "book_id");
+        public static final Column COPY_AVA   = new Column(4,  "copies_available");
+        public static final Column ISBN     = new Column(8,  "isbn");
+        public static final Column PUB_YEAR     = new Column(16, "published_year");
+        public static final Column PUB_ID = new Column(32, "published_id");
+        public static final Column TITLE = new Column(64,"title");
+        private Column(int idx, String name) {
+            super(idx, name);
+            columns.add(this);
+        }
+    }
+    private PreparedStatement createStatement(String sql) {
+        try {
+            return connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
